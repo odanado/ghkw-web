d204ff1c08151da1f847fcca19cb4f4c43918c4e
odan <odan3240@gmail.com>
2019-01-13 23:56:01 +0900

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="text-red-dark">aaa</div>
   </div>
 </template>
 
