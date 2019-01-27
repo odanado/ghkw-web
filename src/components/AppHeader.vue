dbc505b45850476216aac40543842f012e745acf
odan <odan3240@gmail.com>
2019-01-28 03:32:57 +0900

    aタグにhoverを

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,12 +2,12 @@
   <header class="bg-blue px-2">
     <div class="container mx-auto h-full flex items-center ">
       <span class="flex-grow">
-        <router-link class="no-underline text-white" to="/"
+        <router-link class="no-underline text-white hover:opacity-75" to="/"
           >ghkw-web</router-link
         >
       </span>
       <a href="https://github.com/odanado/ghkw-web" target="_blank">
-        <i class="text-white fab fa-github fa-lg" />
+        <i class="text-white hover:opacity-75 fab fa-github fa-lg" />
       </a>
     </div>
   </header>
