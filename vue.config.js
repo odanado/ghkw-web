7dfe8a5587b4982458cca0bcd38924d40d0ea8b7
odan <odan3240@gmail.com>
2019-01-29 21:37:34 +0900

    メタタグを設定

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const package = require("./package.json")
+
+module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        console.log(args)
+        args[0].title = package.name;
+        args[0].description = package.description;
+        args[0].origin = "https://ghkw-web.firebaseapp.com"
+        return args
+      })
+  }
+}
